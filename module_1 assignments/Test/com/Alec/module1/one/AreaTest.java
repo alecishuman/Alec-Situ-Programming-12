2619db32cdc37a4402ee7962611575fe64e892ee
alecishuman <aasitu@gmail.com>
2023-03-08 10:20:32 -0800

    module 1 assignment finished

--- module_1 assignments/Test/com/Alec/module1/one/AreaTest.java
+++ module_1 assignments/Test/com/Alec/module1/one/AreaTest.java
@@ -1,5 +1,6 @@
 package com.Alec.module1.one;
 
+import com.Alec.module1.four.Colour;
 import com.Alec.module1.two.Circle;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,12 @@ class AreaTest {
     void getTriangleArea() {
         Triangle triangle = new Triangle(3, 4, 5);
         assertEquals(6, triangle.getArea());
+        
+        Circle circle = new Circle(6.5);
+        assertEquals(5.5*5.5*Math.PI, circle.getArea());
+
+        TwoDShape rectangle = new TwoDShape(7, 8.1, Colour.RED);
+        assertEquals(56.7, rectangle.getArea());
     }
 
 }
