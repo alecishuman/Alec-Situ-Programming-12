e4f69ff3f733860c324159e8ae50dc5ae0c15377
alecishuman <aasitu@gmail.com>
2023-05-16 10:25:33 -0700

    module 3.4 finished

---
+++ module3.4/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] merge(int[] list1, int[] list2) {
+        int i = 0, j = 0;
+        int[] merged = new int[list1.length + list2.length];
+        while (list1.length + list2.length > 0) {
+            if (list1[i] >= list2[j]) {
+
+            }
+        }
+        return merged;
+    }
+}
