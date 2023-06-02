d876f0a453d2f33923d7de8966c9bab05b1d72e7
alecishuman <aasitu@gmail.com>
2023-06-02 10:11:09 -0700

    Update Module 3.4

--- module3.4/src/Main.java
+++ module3.4/src/Main.java
@@ -1,16 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] x = {6, 4, 2,0};
+        int[] y = {8,7,2,1};
+        System.out.println(merge(x, y));
     }
 
-    public static int[] merge(int[] list1, int[] list2) {
-        int i = 0, j = 0;
-        int[] merged = new int[list1.length + list2.length];
-        while (list1.length + list2.length > 0) {
-            if (list1[i] >= list2[j]) {
+    public static int[] merge(int[] arr1, int[] arr2) {
+        int[] merged = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
 
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                merged[k] = arr1[i];
+                k++;
+                i++;
+            } else {
+                merged[k] = arr2[j];
+                k++;
+                j++;
             }
         }
+
+        while (i < arr1.length) {
+            merged[k] = arr1[i];
+            k++;
+            i++;
+        }
+
+        while (j < arr2.length) {
+            merged[k] = arr2[j];
+            k++;
+            j++;
+        }
+
         return merged;
     }
 }
